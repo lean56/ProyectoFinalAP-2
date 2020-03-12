a283a4f255235d1c70bacad474885f90669e008e
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-12 18:39:16 -0400

    Creando Usuario Models

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/Usuarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string  Nombres { get; set; }
+        public string  NivelUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombres = string.Empty;
+            NivelUsuario = string.Empty;
+            Usuario = string.Empty;
+            Contraseña = string.Empty;
+            FechaIngreso = DateTime.Now;
+        }
+    }
+}
