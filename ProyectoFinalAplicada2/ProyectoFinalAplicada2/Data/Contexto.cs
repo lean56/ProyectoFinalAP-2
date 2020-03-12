132802d3439aecd47c510ee52d6e55476500f209
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-12 18:35:22 -0400

    Creando Contexto

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/ProyectoFinalDb.db");
+        }
+    }
+}
