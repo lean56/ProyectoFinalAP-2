eecbfd38f61f3478c203f204066a4e6b425dbb90
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-12 18:51:19 -0400

    Creando UsuarioController

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Controller/UsuarioController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicada2.Data;
+using ProyectoFinalAplicada2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Controller
+{
+    public class UsuarioController
+    {
+        public bool Guardar(Usuarios usuario)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                if (usuario.UsuarioId == 0)
+                {
+                    paso = Insertar(usuario);
+                }
+
+                else
+
+                if (Buscar(usuario.UsuarioId) == null)
+                {
+                    paso = false;
+                }
+
+                else
+                {
+                    paso = Modificar(usuario);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        private bool Insertar(Usuarios usuario)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Usuarios.Add(usuario);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        private bool Modificar(Usuarios usuario)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Usuarios.Add(usuario);
+                contexto.Entry(usuario).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuario = new Usuarios();
+
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return usuario;
+        }
+
+        public bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+            Usuarios usuario = new Usuarios();
+
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+                contexto.Entry(usuario).State = EntityState.Deleted;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public List<Usuarios> GetList(Expression<Func<Usuarios, bool>> expression)
+        {
+            Contexto contexto = new Contexto();
+            List<Usuarios> lista;
+
+            try
+            {
+                lista = contexto.Usuarios.Where(expression).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return lista;
+        }
+
+    }
+}
--- ProyectoFinalAplicada2/ProyectoFinalAplicada2/Data/Contexto.cs
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Data/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicada2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace ProyectoFinalAplicada2.Data
 {
     public class Contexto : DbContext
     {
+        public DbSet<Usuarios> Usuarios { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Database/ProyectoFinalDb.db");
