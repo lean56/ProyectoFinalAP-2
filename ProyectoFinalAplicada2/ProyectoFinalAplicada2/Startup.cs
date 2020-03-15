9a23e3c8a101a0b66bb90082a3f60862a2d9b62d
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-15 15:54:44 -0400

    .

--- ProyectoFinalAplicada2/ProyectoFinalAplicada2/Startup.cs
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Startup.cs
@@ -52,6 +52,7 @@ namespace ProyectoFinalAplicada2
             services.AddScoped<HttpClient>();
             // BLAZOR COOKIE Auth Code (end)
             // ******
+ 
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
