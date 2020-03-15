ca810e78f638a6c43bb52258dde04c00d284681f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-15 15:48:18 -0400

    Controller de Pagos terminado!

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Controllers/PagosController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicada2.Data;
+using ProyectoFinalAplicada2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Controller
+{
+    public class PagosController
+    {
+        public bool Guardar(Pagos Pago)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+            try
+            {
+                if (Pago.PagoId == 0)
+                {
+                    paso = Insertar(Pago);
+
+                }
+                else
+                {
+                    paso = Modificar(Pago);
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+
+            return paso;
+        }
+
+        private bool Insertar(Pagos Pago)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Pagos.Add(Pago);
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+
+            return paso;
+        }
+
+        private bool Modificar(Pagos Pago)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Entry(Pago).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+
+            return paso;
+        }
+
+        public Pagos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Pagos Pago = new Pagos();
+
+            try
+            {
+                Pago = contexto.Pagos.Find(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+
+            return Pago;
+        }
+
+        public bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+            Pagos Pago = new Pagos();
+
+            try
+            {
+                Pago = contexto.Pagos.Find(id);
+                contexto.Entry(Pago).State = EntityState.Deleted;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return paso;
+        }
+
+        public List<Pagos> GetList(Expression<Func<Pagos, bool>> expression)
+        {
+            Contexto contexto = new Contexto();
+            List<Pagos> ListaPagos;
+
+            try
+            {
+                ListaPagos = contexto.Pagos.Where(expression).ToList();
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return ListaPagos;
+
+        }
+    }
+}
