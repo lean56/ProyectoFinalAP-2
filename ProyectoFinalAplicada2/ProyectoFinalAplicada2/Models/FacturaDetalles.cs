89e512bf4b5cf29568531c9e57b3d33d6beb80b0
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-15 13:02:25 -0400

    Creando Factura Detalles

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/FacturaDetalles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class FacturaDetalles
+    {
+        [Key]
+        public int FacturaDetalleId { get; set; }
+        public int FacturaId { get; set; }
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Importe { get; set; }
+
+        public FacturaDetalles()
+        {
+            FacturaDetalleId = 0;
+            FacturaId = 0;
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Cantidad = 0;
+            Precio = 0;
+            Importe = 0;
+        }
+
+        public FacturaDetalles(int facturaDetalleId, int facturaId, int productoId, string descripcion, int cantidad, decimal precio, decimal importe)
+        {
+            FacturaDetalleId = facturaDetalleId;
+            FacturaId = facturaId;
+            ProductoId = productoId;
+            Descripcion = descripcion;
+            Cantidad = cantidad;
+            Precio = precio;
+            Importe = importe;
+        }
+    }
+}
