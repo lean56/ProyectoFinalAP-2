3c81b1234963fadf76468135ba85de0ab2b89438
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-17 01:21:35 -0400

    Programacion basica del registro de entradas terminada!

--- ProyectoFinalAplicada2/ProyectoFinalAplicada2/Migrations/20200317042254_AgregandoEntrada.Designer.cs
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Migrations/20200317042254_AgregandoEntrada.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinalAplicada2.Data;
 
 namespace ProyectoFinalAplicada2.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20200317042254_AgregandoEntrada")]
+    partial class AgregandoEntrada
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
