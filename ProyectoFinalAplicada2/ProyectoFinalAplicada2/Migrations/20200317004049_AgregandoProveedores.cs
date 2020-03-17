6b66ad0f46590f196dc3b8400802467db49f0200
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-16 21:35:58 -0400

    Programacion basica del registro de pagos terminada!

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Migrations/20200317004049_AgregandoProveedores.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoFinalAplicada2.Migrations
+{
+    public partial class AgregandoProveedores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proveedores",
+                columns: table => new
+                {
+                    ProveedorId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedores", x => x.ProveedorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Proveedores");
+        }
+    }
+}
