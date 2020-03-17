ab5b96577aab364f59be8efb42e68203db20b7f0
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-03-17 11:32:04 -0400

    agregando  toast a factura

--- ProyectoFinalAplicada2/ProyectoFinalAplicada2/Startup.cs
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using System.Net.Http;
+using Blazored.Toast;
 
 namespace ProyectoFinalAplicada2
 {
@@ -22,14 +23,9 @@ namespace ProyectoFinalAplicada2
             Configuration = configuration;
         }
         public IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to 
-        // add services to the container.
-        // For more information on how to configure your application, 
-        // visit https://go.microsoft.com/fwlink/?LinkID=398940
+    
         public void ConfigureServices(IServiceCollection services)
         {
-            // ******
-            // BLAZOR COOKIE Auth Code (begin)
             services.Configure<CookiePolicyOptions>(options =>
             {
                 options.CheckConsentNeeded = context => true;
@@ -38,23 +34,18 @@ namespace ProyectoFinalAplicada2
             services.AddAuthentication(
                 CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie();
-            // BLAZOR COOKIE Auth Code (end)
-            // ******
+   
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            // ******
-            // BLAZOR COOKIE Auth Code (begin)
-            // From: https://github.com/aspnet/Blazor/issues/1554
-            // HttpContextAccessor
+    
             services.AddHttpContextAccessor();
             services.AddScoped<HttpContextAccessor>();
             services.AddHttpClient();
             services.AddScoped<HttpClient>();
-            // BLAZOR COOKIE Auth Code (end)
-            // ******
- 
+            services.AddBlazoredToast();
+
+
         }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -64,22 +55,18 @@ namespace ProyectoFinalAplicada2
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. 
-                // You may want to change this for production scenarios, 
-                // see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
-            // ******
-            // BLAZOR COOKIE Auth Code (begin)
+           
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseAuthentication();
-            // BLAZOR COOKIE Auth Code (end)
-            // ******
+   
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
