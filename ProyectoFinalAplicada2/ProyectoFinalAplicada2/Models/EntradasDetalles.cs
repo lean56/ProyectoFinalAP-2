97a071bf7c51000c69a36c6fea6e326eb71d5827
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-17 00:13:53 -0400

    Modelos Entrada y Entrada Detalle Listos

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/EntradasDetalles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class EntradasDetalles
+    {
+        [Key]
+        public int EntradaDetalleId { get; set; }
+        public int EntradaId { get; set; }
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+
+        public EntradasDetalles()
+        {
+            EntradaDetalleId = 0;
+            EntradaId = 0;
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Cantidad = 0;
+        }
+
+        public EntradasDetalles(int entradaDetalleId, int entradaId, int productoId, string descripcion, int cantidad)
+        {
+            EntradaDetalleId = entradaDetalleId;
+            EntradaId = entradaId;
+            ProductoId = productoId;
+            Descripcion = descripcion;
+            Cantidad = cantidad;
+        }
+    }
+}
