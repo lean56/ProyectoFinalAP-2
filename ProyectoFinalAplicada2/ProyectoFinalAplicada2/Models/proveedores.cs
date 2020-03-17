c3a7f227cd31477c3710ba2154293c7f6c914449
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-16 20:32:25 -0400

    Entidad Proveedores terminada!

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/proveedores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Proveedores
+    {
+        [Key]
+        public int ProveedorId { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Proveedores()
+        {
+            ProveedorId = 0;
+            Nombre = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+        }
+    }
+}
