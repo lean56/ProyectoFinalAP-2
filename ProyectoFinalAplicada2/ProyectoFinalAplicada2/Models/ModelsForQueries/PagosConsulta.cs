66c8e5b3567421cc1f80416cec07e5a8c61e21af
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-18 01:03:36 -0400

    Consulta de Pagos terminada!

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/ModelsForQueries/PagosConsulta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class PagosConsulta
+    {
+        [Key]
+        public int PagoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Cliente { get; set; }
+        public decimal MontoPago { get; set; }
+
+        public PagosConsulta()
+        {
+            PagoId = 0;
+            Fecha = DateTime.Now;
+            Cliente = string.Empty;
+            MontoPago = 0;
+        }
+    }
+}
