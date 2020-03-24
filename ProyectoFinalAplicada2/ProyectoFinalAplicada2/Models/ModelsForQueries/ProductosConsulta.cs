48a3ee71d87d027105f36d85b42577b21f06f0c4
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-03-24 11:51:26 -0400

    Consulta de Productos terminada!

---
+++ ProyectoFinalAplicada2/ProyectoFinalAplicada2/Models/ModelsForQueries/ProductosConsulta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models.ModelsForQueries
+{
+    public class ProductosConsulta
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descrpcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Categoria { get; set; }
+        public string Proveedor { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public ProductosConsulta()
+        {
+            ProductoId = 0;
+            Descrpcion = string.Empty;
+            FechaCreacion = DateTime.Now;
+            Categoria = string.Empty;
+            Proveedor = string.Empty;
+            Precio = 0;
+            Cantidad = 0;
+        }
+    }
+}
